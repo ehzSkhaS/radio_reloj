5512a4508bf6ace72678188db9df2d6710eeede7
ehzSkhaS <ehz.skhas@gmail.com>
2021-03-12 19:27:45 -0500

    No longer double tick on sync
    - simpleaudio (lag on short audio, no longer in use)
    - pygame.mixer (on linux depends: libsdl2-mixer-2.0.0)

--- clock_ticks.py
+++ clock_ticks.py
@@ -1,51 +1,43 @@
-#!/usr/bin/python3.6
-
-import sys
 import time
-import numpy as np
-import simpleaudio as sa
-from subprocess import *
+import pygame as gm
 from datetime import datetime
-from multiprocessing import Process
+from functools import lru_cache
+
 
 class Generator():
-    sec_changed = 'dead'        
-
+    @lru_cache(maxsize=None)
     def __init__(self, host, sync_time):
-        self.host = host              
-        self.sync_time = sync_time     
-        # Dictionary of:       [freq, fs,    secs]
-        self.ntype =    {"1" : [1000, 44100, 0.01],  # secs
-                         "2" : [1000, 44100, 0.125], # mins 1, 2, 3, 4  
-                         "3" : [1000, 44100, 0.25],  # mins 0, 5
-                         "4" : [1000, 44100, 0.05],  # .
-                         "5" : [1000, 44100, 0.15]}  # _
-        # Dictionary of:       audio                      
-        self.naudio =   {"1" : self.notes_build("1"),
-                         "2" : self.notes_build("2"),
-                         "3" : self.notes_build("3"),
-                         "4" : self.notes_build("4"),
-                         "5" : self.notes_build("5")}      
-        #self.sys_timer_sync(host, sync_time)
+        self.host = host
+        self.sync_time = sync_time
+        gm.mixer.init()
+        # List of: audio
+        self.naudio = [gm.mixer.Sound('sounds/tick.wav'),
+                       gm.mixer.Sound('sounds/min_1_4.wav'),
+                       gm.mixer.Sound('sounds/min_0_5.wav'),
+                       gm.mixer.Sound('sounds/rr.wav')]
+        # self.sys_timer_sync(host, sync_time)
 
     def sys_timer_sync(self, host, sync_time):
-        import time_sync_ntp as tsn  
+        import sys
+        import time_sync_ntp as tsn
+        from multiprocessing import Process
         p = 0
-        if sys.platform == "win32":      
-            p = Process(target = tsn.set_windows_time, 
-                        args = (host, sync_time))
-        if sys.platform.startswith('linux') :      
-            p = Process(target = tsn.set_linux_time, 
-                        args = (host, sync_time))
-        p.start()            
+        if sys.platform == "win32":
+            p = Process(target=tsn.set_windows_time,
+                        args=(host, sync_time))
+        if sys.platform.startswith('linux'):
+            p = Process(target=tsn.set_linux_time,
+                        args=(host, sync_time))
+        p.start()
 
-    def notes_build(self, t_note):        
+    def notes_build(self, t_note):
+        import numpy as np
         # Distibute the tick by fs
-        t = np.linspace(0, 
-                        self.ntype[t_note][2], 
-                        int(self.ntype[t_note][2] * self.ntype[t_note][1]), 
+        t = np.linspace(0,
+                        self.ntype[t_note][2],
+                        int(self.ntype[t_note][2] * self.ntype[t_note][1]),
                         False)
-        # Generate a 440 Hz sine wave
+        # Generate a 1000 Hz sine wave
         note = np.sin(self.ntype[t_note][0] * t * 2 * np.pi)
         # Ensure that highest value is in 16-bit range
         audio = note * (2**15 - 1) / np.max(np.abs(note))
@@ -53,30 +45,22 @@ class Generator():
         audio = audio.astype(np.int16)
         return audio
 
-    def start_palyback(self, t_note):
-        # Start playback        
-        play_obj = sa.play_buffer(self.naudio[t_note], 
-                                  1, 
-                                  2, 
-                                  self.ntype[t_note][1])
-        # Wait for playback to finish before exiting
-        play_obj.wait_done()
-
-    def get_sys_time_data(self):
-        left_micro = (1000000 - datetime.now().microsecond) / 1000000 
-        time.sleep(left_micro)
+    def emit(self):
         t = datetime.now()
-        print(t.hour,":",t.minute,":",t.second,":",t.microsecond)
-        t = datetime.now()        
-        if (t.minute % 5) == 0 and t.second == 0:
-            self.start_palyback("3")
-        elif (t.minute % 5) != 0 and t.second == 0:
-            self.start_palyback("2")
-        else:
-            self.start_palyback("1")
+        if t.microsecond < 25000:
+            if t.minute % 5 == 0 and t.second == 0:
+                self.naudio[2].play()
+                print(t.hour, ":", t.minute, ":", t.second, ":", t.microsecond)
+            elif t.minute % 5 != 0 and t.second == 0:
+                self.naudio[1].play()
+                print(t.hour, ":", t.minute, ":", t.second, ":", t.microsecond)
+            else:
+                self.naudio[0].play()
+                print(t.hour, ":", t.minute, ":", t.second, ":", t.microsecond)
+        time.sleep((1000000 - datetime.now().microsecond) * 0.000001)
 
-if __name__ == "__main__":    
-    gen = Generator("pool.ntp.org", 17)                    
-    while 1:                
-        gen.get_sys_time_data()   
-    
+
+if __name__ == "__main__":
+    gen = Generator("pool.ntp.org", 5)
+    while 1:
+        gen.emit()
